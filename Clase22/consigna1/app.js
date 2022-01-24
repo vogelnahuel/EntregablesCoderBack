ab6968325d550207e7d2b2259b2750e7b5e28e28
vogel <vogelnahuel@gmail.com>
2022-01-24 17:58:18 -0300

    consigna 1 del entregable 22 terminada y consigna 2 empezada

---
+++ Clase22/consigna1/app.js
@@ -0,0 +1,172 @@
+const express = require("express");
+const { Router } = express;
+const app = express();
+const router = Router();
+const { filtrar } = require("./utils");
+
+
+
+const { Server: HttpServer } = require("http");
+const { Server: IOServer } = require("socket.io");
+const httpServer = new HttpServer(app);
+const io = new IOServer(httpServer);
+
+//inicializacion de variables donde se guardan id y los productos
+const productos = [
+  {
+    id: 1,
+    title: "Fideos",
+    number: 75.6,
+    thumbnail:
+      "https://http2.mlstatic.com/D_NQ_NP_626878-MLA42886753623_072020-O.jpg",
+  },
+  {
+    id: 2,
+    title: "Atun",
+    number: 590.6,
+    thumbnail:
+      "https://ardiaprod.vteximg.com.br/arquivos/ids/184140-1000-1000/Atun-en-Aceite-La-Campagnola-170-Gr-_1.jpg?v=637427537426970000",
+  },
+  {
+    id: 3,
+    title: "Arroz",
+    number: 750,
+    thumbnail: "https://maxiconsumo.com/pub/media/catalog/product/cache/c687aa7517cf01e65c009f6943c2b1e9/5/6/569.jpg",
+  },
+];
+
+
+let id = 3;
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const Archivo = require('./Archivo.js')
+const archivo = new Archivo();
+const ruta = 'producto.txt';
+const codificacion = 'utf-8';
+const ProductApi = require('./productsApi')
+
+//base de datos
+const Contenedor = require('./Daos/MensajesMongo.js');
+const consultas = new Contenedor();
+
+//sockets
+httpServer.listen(process.env.PORT || 8080, () => {
+  console.log("SERVER ON");
+});
+
+
+
+io.on("connection", async (socket) => {
+
+  // socket.on("getProducts", async (obj) => {
+
+  //   await consultas.add(obj)
+  //   const arrayProductos  = await consultas.getAll();
+  //   console.log(arrayProductos)
+  //   io.emit("productList", arrayProductos);
+  // });
+
+  socket.on("getMensaje", async (obj) => {
+     await consultas.add(obj)
+     const arrayProductos  = await consultas.getAll();
+     console.log("Emitir",arrayProductos)
+     io.emit("mensajesList", arrayProductos);
+  });
+
+});
+
+
+
+
+//FAKER entregable 22
+
+app.get("/api/productos-test", (req, res) => {
+  const obj   = new ProductApi();
+  for (let index = 0; index < 5; index++) {
+    obj.generateProducts()
+  }
+   return res.json(obj.get());
+});
+
+
+
+
+router.get("/", (req, res) => {
+  return res.render("list", { productos });
+});
+
+router.get("/:id", (req, res, next) => {
+  const idParam = parseInt(req.params.id);
+  const filtrado = filtrar(productos, idParam);
+  if (filtrado?.httpStatusCode) {
+    return next(filtrado);
+  }
+  res.json(filtrado);
+});
+
+//mandar como nombre thumbnail  el campo si se utiliza desde postman la key para el File
+router.post("/" , (req, res ) => {
+ 
+  id++;
+  const { title, price,thumbnail } = req.body;
+  productos.push({ title, price, thumbnail, id });
+
+  return res.redirect("/list");
+});
+router.put("/:id", (req, res) => {
+  const file = req.file;
+  const idParam = parseInt(req.params.id);
+  const filtrado = filtrar(productos, idParam);
+  if (filtrado?.httpStatusCode) {
+    return next(filtrado);
+  }
+
+  const { title, price } = req.body;
+
+  //solamente cambio los pasados por parametro y si no estan dejo los que ya estaban
+  const titleInsert = title ? title : filtrado[0].title;
+  const priceInsert = price ? price : filtrado[0].price;
+  const fileInsert = file ? file : filtrado[0].thumbnail;
+
+  //actualizo el array en la posicion pasada
+  productos[idParam - 1] = {
+    title: titleInsert,
+    price: priceInsert,
+    file: fileInsert,
+    id: idParam,
+  };
+  res.json({
+    title: titleInsert,
+    price: priceInsert,
+    file: fileInsert,
+    id: idParam,
+  });
+});
+
+router.delete("/:id", (req, res, next) => {
+  const idParam = parseInt(req.params.id);
+  const eliminado = filtrar(productos, idParam);
+  if (eliminado?.httpStatusCode) {
+    return next(eliminado);
+  }
+  productos.splice(idParam - 1, 1); //elimino del array
+  res.json({ eliminado });
+});
+
+app.get("/", (req, res) => {
+  res.render("formulario");
+});
+app.get("/list", (req, res) => {
+  return res.render("list", { productos });
+});
+
+//donde se van a guardar las imagenes
+app.use(express.static("public"));
+//ruta por defecto
+app.use("/productos", router);
---
+++ Clase22/consigna1/public/index.js
@@ -0,0 +1,71 @@
+const socket = io();
+
+// socket.on("productList", data => {
+    
+  
+//    const tbody=  document.querySelector('#tbody')
+//    const htmlData = data.map((value) => {
+//     return `
+//         <tr>
+//             <td>${value.nombre}</td>
+//             <td>${value.precio}</td>
+//             <td><img class='img-thumbnail' src='${value.foto}' style="width:100px;"> </td>
+//         </tr> `
+//     }).join(' ');
+
+//     tbody.innerHTML = htmlData; 
+// })
+
+// document.querySelector('#emitirProducto').addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     let nombre = document.querySelector('#title').value;
+//     let precio = document.querySelector('#number').value;
+//     let foto = document.querySelector('#thumbnail').value;
+
+
+//     socket.emit("getProducts", {nombre,precio,foto});
+
+//     title= "";
+//     number = "";
+//     thumbnail = "";
+
+// })
+
+
+document.querySelector('#emitirMensaje').addEventListener('click',(e)=>{
+    e.preventDefault();
+    let email = document.querySelector('#Email').value;
+    let nombre = document.querySelector('#Nombre').value;
+    let apellido = document.querySelector('#Apellido').value;
+    let edad = document.querySelector('#Edad').value;
+    let alias = document.querySelector('#Alias').value;
+    let avatar = document.querySelector('#Avatar').value;
+    let mensaje = document.querySelector('#inputMensaje').value;
+    socket.emit("getMensaje", {author:{id:email,nombre,apellido,edad,alias,avatar} ,text:mensaje} );
+    mensaje="";
+})
+
+
+socket.on("mensajesList", data => {
+    
+  
+    const div=  document.querySelector('#mensajes')
+    const htmlData = data.map((value) => {
+     return `
+            <div class="display-flex">
+                <p class="email">${value.author.id}</p>
+                <p class="email">${value.author.nombre}</p>
+                <p class="email">${value.author.apellido}</p>
+                <p class="email">${value.author.edad}</p>
+                <p class="email">${value.author.alias}</p>
+                <p class="email">${value.author.avatar}</p>
+                <p class="mensaje">${value.text}</p>
+            </div>
+          `
+     }).join(' ');
+ 
+     div.innerHTML = htmlData; 
+ })
+
+
+
